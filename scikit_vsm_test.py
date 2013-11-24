45ea785603bfcaa1f41a4ad538e70d526e6f7112
Will <whp3652@rit.edu>
2013-11-24 01:32:28 -0500

    experiment further with sklearn, and cos similarity

--- scikit_vsm_test.py
+++ scikit_vsm_test.py
@@ -1,10 +1,17 @@
 from sklearn.feature_extraction.text import CountVectorizer
-vectorizer = CountVectorizer()
+from sklearn.feature_extraction.text import TfidfTransformer
 
 train_set = ("The sky is blue.", "The sun is bright.")
 test_set = ("The sun in the sky is bright.", "We can see the shining sun, the bright sun.")
 
-print vectorizer.fit_transform(train_set)
+count_vectorizer = CountVectorizer()
+count_vectorizer.fit_transform(train_set)
 
-tmatrix = vectorizer.transform(test_set)
-print tmatrix
+freq_matrix = count_vectorizer.transform(test_set)
+freq_matrix.todense()
+
+tf_idf = TfidfTransformer()
+tf_idf.fit(freq_matrix)
+
+tf_idf_matrix = tf_idf.transform(freq_matrix)
+tf_idf_matrix.todense()
---
+++ scikit_vsm_test2.py
@@ -0,0 +1,15 @@
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
+documents = ("The sky is blue",
+			"The sun is bright",
+			"The sun in the sky is bright",
+			"We can see the shining sun, the bright sun")
+
+tfidf_vectorizer = TfidfVectorizer()
+tfidf_matrix = tfidf_vectorizer.fit_transform(documents)
+#print tfidf_matrix
+
+print cosine_similarity(tfidf_matrix[0:1], tfidf_matrix)
+
+#x_test = vectorizer.transform(test_set)
