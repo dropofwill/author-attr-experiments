a0648fe7881a7b0130dc4891ce9c10c7d5aac40d
Will <whp3652@rit.edu>
2013-11-27 23:31:57 -0500

    Finish implementation of Multinomial Naive Bayes grid search with basic count vector

--- multinomialNB_cV.py
+++ multinomialNB_cV.py
@@ -1,16 +1,19 @@
 from sklearn import datasets
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.feature_extraction.text import CountVectorizer
 
 from sklearn.cross_validation import train_test_split
 from sklearn.cross_validation import cross_val_score
 from sklearn.cross_validation import ShuffleSplit
 from sklearn.cross_validation import Bootstrap
+
 from sklearn.naive_bayes import MultinomialNB
+from sklearn.grid_search import GridSearchCV
 
 from scipy.stats import sem
+from pprint import pprint
 import numpy as np
+import pylab as pl
 
 # Calculates the mean of the scores with the standard deviation
 def mean_sem(scores):
@@ -22,15 +25,52 @@ docs = datasets.load_files(container_path="../../sklearn_data/problemC/")
 X, y = docs.data, docs.target
 
 # Select Features via Bag of Words approach without stop words
-X = CountVectorizer(charset_error='ignore', stop_words='english').fit_transform(X)
+X = CountVectorizer(charset_error='ignore', stop_words='english', strip_accents='unicode', ).fit_transform(X)
 n_samples, n_features = X.shape
 
 
+# sklearn's grid search
+parameters = { 
+	'alpha': np.logspace(-25,0,25)
+}
+
+	#pprint(parameters)
+
+
+bv = Bootstrap(n_samples, n_iter=25, test_size=0.3, random_state=42)
+mnb_gv = GridSearchCV(MultinomialNB(), parameters, cv=bv,)
+mnb_gv.fit(X, y)
+print mnb_gv.best_params_
+print mnb_gv.best_score_
+
+mnb_best_score = mnb_gv.best_score_
+
+
+# Grid Search implementation
+'''
+n_alphas = 10
+n_iter = 10
+cv = Bootstrap(n_samples, n_iter=n_iter)
+train_scores = np.zeros((n_alphas, n_iter))
+test_scores = np.zeros((n_alphas, n_iter))
+alphas = np.logspace(-7,-1,n_alphas)
+print alphas
+
+for i, alpha in enumerate(alphas):
+	for j, (train, test) in enumerate(cv):
+		clf = MultinomialNB(alpha=alpha).fit(X[train], y[train])
+		train_scores[i, j] = clf.score(X[train], y[train])
+		test_scores[i, j] = clf.score(X[test], y[test])
+'''
+
+
 # CV with Bootstrap
-mnb = MultinomialNB()
-bv = Bootstrap(n_samples, n_iter=100, test_size=0.2, random_state=0)
+
+mnb = MultinomialNB(alpha=mnb_best_score)
+bv = Bootstrap(n_samples, n_iter=100, test_size=0.2, random_state=42)
 boot_scores = cross_val_score(mnb, X, y, cv=bv)
 print mean_sem(boot_scores)
+
 
 
 # CV with ShuffleSpit
@@ -39,6 +79,7 @@ cv = ShuffleSplit(n_samples, n_iter=100, test_size=0.2, random_state=0)
 test_scores = cross_val_score(mnb, X, y, cv=cv)
 print np.mean(test_scores)
 '''
+
 
 # Single run through
 '''
